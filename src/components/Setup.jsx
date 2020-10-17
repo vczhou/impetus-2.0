6e964862c2ddf07fb908161f7fa00b45fa91a12a
michelle-gu <tianxin.gu@gmail.com>
2020-10-17 15:19:14 -0500

    Setup page basics and antd ui framework

--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -1,19 +1,68 @@
 import React from 'react'
+import { Typography, Form, Input, Button } from 'antd';
 
-class Setup extends React.Component {
-  constructor(props) {
+const { Title } = Typography;
 
-  }
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
 
-  render() {
-    return (
-      <div className="setup-page">
-        <h1>Setup</h1>
-        <ul>
-          <li>Name</li>
-          <li>Birthday</li>
-        </ul>
-      </div>
-    );
-  }
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+function Setup(props) {
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div className="setup-page">
+      <Title>Let's get to know you.</Title>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="What's your name?"
+          name="name"
+          rules={[{ required: true, message: 'Please input your name!' }]}
+        >
+        <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Email?"
+          name="email"
+          rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Birthday?"
+          name="birthday"
+          rules={[{ required: true, message: 'Please input your birthday!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Get Started
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
+
+export default Setup;
