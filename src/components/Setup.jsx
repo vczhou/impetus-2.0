337b045110e9856142b0cf5deaeca99cb7f46852
michelle-gu <tianxin.gu@gmail.com>
2020-10-17 14:21:37 -0500

    Components folder and start setup page

---
+++ src/components/Setup.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Setup extends React.Component {
+  constructor(props) {
+
+  }
+
+  render() {
+    return (
+      <div className="setup-page">
+        <h1>Setup</h1>
+        <ul>
+          <li>Name</li>
+          <li>Birthday</li>
+        </ul>
+      </div>
+    );
+  }
+}
