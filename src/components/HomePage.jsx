c56473279ab3bb87e8f5c5a94ee9a8610640242c
michelle-gu <tianxin.gu@gmail.com>
2020-10-17 17:53:17 -0500

    Reducer attempt?

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import Clock from './Clock';
 import Setup from './Setup';
 import WelcomeMessage from './WelcomeMessage';
 
-function HomePage() {
-  const [hasCompletedSetup, setHasCompletedSetup] = useState(false);
-  const [name, setName] = useState('');
-
-  function completeSetup(name) {
-    setHasCompletedSetup(true);
-    setName(name);
-    return hasCompletedSetup;
+function homePageReducer(state, action) {
+  switch (action.type) {
+    case 'COMPLETE_SETUP':
+      return [
+        ...state, 
+        {
+          hasCompletedSetup: true,
+          name: action.name
+        }
+      ];
+    default:
+      return state;
   }
+}
 
-	if (!hasCompletedSetup) {
+function HomePage() {
+  const [state, dispatch] = useReducer(
+    homePageReducer,
+    {
+      hasCompletedSetup: false,
+      name: ''
+    }
+  );
+
+	if (!state.hasCompletedSetup) {
     return (
-      <Setup completeSetup={(name) => completeSetup(name)} />
+      <Setup completeSetup={(name) => dispatch({ type: 'COMPLETE_SETUP', name: name})} />
     );
   }
 
@@ -23,7 +37,7 @@ function HomePage() {
     <div className="App">
       <header className="App-header">
         <Clock />
-        <WelcomeMessage name={name} />
+        <WelcomeMessage name={state.name} />
       </header>
     </div>
   );
