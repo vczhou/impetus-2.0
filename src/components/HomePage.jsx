9e3d8d709c86ec98cc95df3225dc4a51fd3a05a7
vczhou <victoria.ch.zhou@gmail.com>
2020-10-17 19:52:01 -0500

    calendar widget

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,12 +5,14 @@ import WelcomeMessage from './WelcomeMessage';
 import Mantra from './Mantra';
 import { Popover, Button } from 'antd';
 import TodoWidget from './TodoWidget';
+import CalendarWidget from './CalendarWidget';
 
 function HomePage() {
   const [hasCompletedSetup, setHasCompletedSetup] = useState(false);
   const [name, setName] = useState('');
 
   const todoWidget = <TodoWidget />;
+  const calendarWidget = <CalendarWidget />;
   const fillerContent = <div>Some filler content!</div>;
 
   function completeSetup(name) {
@@ -33,30 +35,30 @@ function HomePage() {
       <Popover content={todoWidget} title="To Do" trigger="click">
         <Button>To Do</Button>
       </Popover>
-      <Popover content={fillerContent} title="To Do" trigger="click">
+      <Popover content={fillerContent} title="Pomodoro Timer" trigger="click">
         <Button>Pomodoro</Button>
       </Popover>
-      <Popover content={fillerContent} title="To Do" trigger="click">
+      <Popover content={calendarWidget} title="Calendar" trigger="click">
         <Button>Calendar</Button>
       </Popover>
-      <Popover content={fillerContent} title="To Do" trigger="click">
+      <Popover content={fillerContent} title="Weather" trigger="click">
         <Button>Weather</Button>
       </Popover>
 
-      <Popover content={fillerContent} title="To Do" trigger="click">
+      <Popover content={fillerContent} title="Devotional" trigger="click">
         <Button>Devotional</Button>
       </Popover>
-      <Popover content={fillerContent} title="To Do" trigger="click">
+      <Popover content={fillerContent} title="Praise Journal" trigger="click">
         <Button>Praise Journal</Button>
       </Popover>
-      <Popover content={fillerContent} title="To Do" trigger="click">
+      <Popover content={fillerContent} title="Prayer" trigger="click">
         <Button>Prayer</Button>
       </Popover>
-      <Popover content={fillerContent} title="To Do" trigger="click">
+      <Popover content={fillerContent} title="Missions & Giving" trigger="click">
         <Button>Missions/Give</Button>
       </Popover>
       
-      <Popover content={fillerContent} title="To Do" trigger="click">
+      <Popover content={fillerContent} title="Settings" trigger="click">
         <Button>Settings</Button>
       </Popover>
     </div>
