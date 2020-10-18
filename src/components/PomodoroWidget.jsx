4cf59d7d4be8a817b1ac779cb4612a446061e129
vczhou <victoria.ch.zhou@gmail.com>
2020-10-18 11:46:00 -0500

    pomodoro reset and oncomplete functionality

--- src/components/PomodoroWidget.jsx
+++ src/components/PomodoroWidget.jsx
@@ -1,20 +1,38 @@
-import  React, { useRef} from 'react';
+import  React, { useRef, useState } from 'react';
 import Countdown from 'react-countdown';
 import { Button } from 'antd';
 
 
 function PomodoroWidget(props) {
+  const timeInSec = 1500000;
+  const [currentTime, setCurrentTime] = useState(Date.now() + timeInSec);
   const clockRef = useRef();
-  const handleStart = () => clockRef.current.start();
-  const handlePause = () => clockRef.current.pause();
+
+  const handleStart = () => {
+    clockRef.current.start();
+  }
+  const handlePause = () => {
+    clockRef.current.pause();
+  }
+  const handleReset = () => {
+    setCurrentTime(Date.now() + timeInSec)
+  }
 
   return (
     <div className="pomodoro-widget">
-      <Countdown autoStart={false} date={Date.now() + 1500000} ref={clockRef} />
+      <Countdown 
+        autoStart={false}
+        date={currentTime}
+        ref={clockRef} 
+        onComplete={() => {
+          // reset current time
+          setCurrentTime(Date.now() + timeInSec);
+        }}
+      />
       <div>
         <Button onClick={handleStart}>Start</Button>
         <Button onClick={handlePause}>Stop</Button>
-        <Button>Reset</Button>
+        <Button onClick={handleReset}>Reset</Button>
       </div>
     </div>
   );
