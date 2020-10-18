cc75f11366e7aa704488452179987c9e3ca86ca9
vczhou <victoria.ch.zhou@gmail.com>
2020-10-18 11:33:39 -0500

    start/ pause for pomodoro

--- src/components/PomodoroWidget.jsx
+++ src/components/PomodoroWidget.jsx
@@ -1,15 +1,19 @@
-import  React from 'react';
+import  React, { useRef} from 'react';
 import Countdown from 'react-countdown';
 import { Button } from 'antd';
 
 
 function PomodoroWidget(props) {
+  const clockRef = useRef();
+  const handleStart = () => clockRef.current.start();
+  const handlePause = () => clockRef.current.pause();
+
   return (
     <div className="pomodoro-widget">
-      <Countdown autostart={false} date={Date.now() + 1500000} />
+      <Countdown autoStart={false} date={Date.now() + 1500000} ref={clockRef} />
       <div>
-        <Button>Start</Button>
-        <Button>Stop</Button>
+        <Button onClick={handleStart}>Start</Button>
+        <Button onClick={handlePause}>Stop</Button>
         <Button>Reset</Button>
       </div>
     </div>
